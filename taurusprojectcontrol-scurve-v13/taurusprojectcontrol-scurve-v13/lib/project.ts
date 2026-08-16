fce93549406aa6808593af8af4a999fb7e8bdbea
sarwar159200-hue <sarwar.159200@gmail.com>
2026-08-16 13:24:40 +0300

    Delete taurusprojectcontrol-scurve-v13/taurusprojectcontrol-scurve-v13 directory

--- taurusprojectcontrol-scurve-v13/taurusprojectcontrol-scurve-v13/lib/project.ts
+++
@@ -1,6 +0,0 @@
-const UUID_PATTERN = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
-
-export function getDefaultProjectId() {
-  const value = process.env.DEFAULT_PROJECT_ID ?? "";
-  return UUID_PATTERN.test(value) ? value : null;
-}
